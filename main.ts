11cd58db50e68dcee7315862a7ab10f9ba45e78f
Jon Jampen <dev@jonjampen.ch>
2024-05-04 11:27:50 +0200

    Adding button to file explorer to directly create a new Xournal++ note.

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { TFile, Plugin, Notice } from 'obsidian';
+import { TFile, Plugin, Notice, ButtonComponent } from 'obsidian';
 import { createXoppFileModal } from 'src/modal';
 import { createPdfToolbarButton } from 'src/pdfToolbarButton';
 import { annotatePdfInXournalpp, openXournalppFile } from 'src/xoppActions';
@@ -46,6 +46,15 @@ export default class XoppPlugin extends Plugin {
 				.setTitle("Create a new Xournal++ note")
 				.open()
 		});
+
+		// on startup
+		this.registerEvent(this.app.workspace.on("layout-change", () => {
+			this.addCreateXournalppNavIcon()
+		}))
+
+		this.registerEvent(this.app.workspace.on('active-leaf-change', (leaf) => {
+			if (leaf?.getDisplayText() === "Files") this.addCreateXournalppNavIcon()
+		}));
     }
 
 	onFileOpen = async (file: TFile) => {
@@ -122,6 +131,30 @@ export default class XoppPlugin extends Plugin {
 						.open()
 				});
 		});
+	}
+
+	addCreateXournalppNavIcon = () => {
+		let fileExplorers = this.app.workspace.getLeavesOfType("file-explorer")
+
+		fileExplorers.forEach(fileExplorer => {
+			let fileExplorerIconsNav = fileExplorer.view.headerDom.navButtonsEl;
+			
+			if (fileExplorerIconsNav && fileExplorerIconsNav.children.length > 2 && !fileExplorerIconsNav.querySelector('.xournalpp-create-icon')) {
+				let createXoppButton = new ButtonComponent(fileExplorerIconsNav as HTMLElement)
+					.setClass("clickable-icon")
+					.setClass("xournalpp-create-icon")
+					.setIcon('pen-tool')
+					.setTooltip('Create new Xournal++');
+				
+				createXoppButton.onClick(() =>  {
+					new createXoppFileModal(this.app, this)
+					.setTitle("Create a new Xournal++ note")
+					.open()
+				})
+				
+				fileExplorerIconsNav.insertAfter(createXoppButton.buttonEl, fileExplorerIconsNav.children[1]);
+			}
+		})
 	}
 
     onunload() {}
