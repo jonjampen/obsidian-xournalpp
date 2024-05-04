e8272a76b87ffb589636dbc5f3dde3cbc91e0da0
Jon Jampen <dev@jonjampen.ch>
2024-05-04 12:28:43 +0200

    Moving pdf toolbar to separate file.

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
-import { TFile, Plugin, Notice, ButtonComponent } from 'obsidian';
+import { Plugin, ButtonComponent } from 'obsidian';
 import { createCommands } from 'src/commands';
 import { setupListeners } from 'src/listeners';
 import { createXoppFileModal } from 'src/modal';
-import { createPdfToolbarButton } from 'src/pdfToolbarButton';
 import { createRibbonIcons } from 'src/ribbonIcons';
 
 export default class XoppPlugin extends Plugin {
@@ -11,31 +10,6 @@ export default class XoppPlugin extends Plugin {
 		createCommands(this);
 		createRibbonIcons(this);
     }
-
-	onFileOpen = async (file: TFile) => {
-		if (file && file instanceof TFile && file.extension === "pdf") {
-			// check if related xopp file exists
-			let filename = file?.name;
-			let xoppFilename = filename?.replace(".pdf", ".xopp");
-
-			// set parent folder or root vault folder
-			const parentFolder = file?.parent ?? this.app.vault.getFolderByPath("/");
-			const xoppFile = parentFolder?.children.find((child) => child.name === xoppFilename) as TFile
-
-			// reset icon
-			const oldIcons = document.querySelectorAll('.xournalpp-open-icon');
-			oldIcons.forEach((old) => old.remove())
-
-			if (xoppFile) {
-				// show toolbar icon
-				const pdfToolbar = document.querySelector('.pdf-toolbar-right') as HTMLElement;
-
-				if (pdfToolbar) {
-					createPdfToolbarButton(pdfToolbar, xoppFile, this.app)
-				}
-			}
-		}
-	}
 
 	addCreateXournalppNavIcon = () => {
 		let fileExplorers = this.app.workspace.getLeavesOfType("file-explorer")
---
+++ src/pdfToolbar.ts
@@ -0,0 +1,40 @@
+import XoppPlugin from "main";
+import { TFile } from "obsidian";
+import { findCorrespondingXoppToPdf } from "./xoppActions";
+import { ButtonComponent, App } from 'obsidian';
+import { openXournalppFile } from './xoppActions';
+
+export function addOpenInXournalppToPdfToolbar(file: TFile, plugin: XoppPlugin) {
+    if (file && file.extension === "pdf") {
+        // reset icon
+        const oldIcons = document.querySelectorAll('.xournalpp-open-icon');
+        oldIcons.forEach((old) => old.remove())
+
+        let xoppFile = findCorrespondingXoppToPdf(file.path, plugin)
+
+        if (xoppFile) {
+            // show toolbar icon
+            const pdfToolbar = document.querySelector('.pdf-toolbar-right') as HTMLElement;
+
+            if (pdfToolbar) {
+                createPdfToolbarButton(pdfToolbar, xoppFile, plugin.app)
+            }
+        }
+    }
+}
+
+
+export function createPdfToolbarButton(pdfToolbar: HTMLElement, xoppFile: TFile, app: App): void {
+    let xoppButton = new ButtonComponent(pdfToolbar)
+        .setClass("clickable-icon")
+        .setClass("xournalpp-open-icon")
+        .setButtonText('Edit in Xournal++')
+        .setIcon('pen-tool')
+        .setTooltip('Open in Xournal++');
+
+        xoppButton.onClick(() =>  {
+            openXournalppFile(xoppFile, app);
+        })
+
+        pdfToolbar.appendChild(xoppButton.buttonEl);
+}
