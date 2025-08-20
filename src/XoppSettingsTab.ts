2c276747583a9755d80ea59bc1cf4fa6a8f4d088
Jon Jampen <dev@jonjampen.ch>
2025-08-20 21:57:20 +0200

    Added live example and icon to modal with list of supported shorthands

--- src/XoppSettingsTab.ts
+++ src/XoppSettingsTab.ts
@@ -2,6 +2,9 @@ import XoppPlugin from "main";
 import { App, Setting, PluginSettingTab } from "obsidian";
 import ConfirmationModal from "./modals/ConfirmationModal";
 import { exportAllXoppToPDF } from "src/xopp2pdf";
+import parseFileName from "./fileNameParser";
+import { shorthands } from "./shorthands";
+import { ShorthandHelpModal } from "./modals/ShorthandHelpModal";
 
 export class XoppSettingsTab extends PluginSettingTab {
     plugin: XoppPlugin;
@@ -10,58 +13,60 @@ export class XoppSettingsTab extends PluginSettingTab {
         super(app, plugin);
         this.plugin = plugin;
     }
-  
+
     display(): void {
         let { containerEl } = this;
-    
+
         containerEl.empty();
-    
+
         new Setting(containerEl)
-    .setName("Auto export Xournal++ files")
-    .setDesc("Automatically export Xournal++ files to PDF upon modification.")
-    .addToggle((toggle) => {
-        toggle
-            .setValue(this.plugin.settings.autoExport)
-            .onChange(async (value) => {
-                if (value) {
-                    const initialValue = this.plugin.settings.autoExport;
-                    const confirmationModal = new ConfirmationModal(this.app, async () => {
-                        this.plugin.settings.autoExport = true;
-                        await this.plugin.saveSettings();
-                        toggle.setValue(true);
-                    
-                        exportAllXoppToPDF(this.plugin);
-                    }, async () => {
+            .setName("Auto export Xournal++ files")
+            .setDesc("Automatically export Xournal++ files to PDF upon modification.")
+            .addToggle((toggle) => {
+                toggle.setValue(this.plugin.settings.autoExport).onChange(async (value) => {
+                    if (value) {
+                        const initialValue = this.plugin.settings.autoExport;
+                        const confirmationModal = new ConfirmationModal(
+                            this.app,
+                            async () => {
+                                this.plugin.settings.autoExport = true;
+                                await this.plugin.saveSettings();
+                                toggle.setValue(true);
+
+                                exportAllXoppToPDF(this.plugin);
+                            },
+                            async () => {
+                                this.plugin.settings.autoExport = false;
+                                await this.plugin.saveSettings();
+                                toggle.setValue(false);
+                            },
+                            initialValue,
+                            toggle
+                        );
+                        confirmationModal.onClose = () => {
+                            if (!confirmationModal.confirmed) {
+                                toggle.setValue(false);
+                            }
+                        };
+                        confirmationModal.open();
+                    } else {
                         this.plugin.settings.autoExport = false;
                         await this.plugin.saveSettings();
-                        toggle.setValue(false); 
-                    }, initialValue, toggle);
-                    confirmationModal.onClose = () => {
-                        if (!confirmationModal.confirmed) {
-                            toggle.setValue(false);
-                        }
-                    };
-                    confirmationModal.open();
-                } else {
-                    this.plugin.settings.autoExport = false;
-                    await this.plugin.saveSettings();
-                    toggle.setValue(false);
-                }
+                        toggle.setValue(false);
+                    }
+                });
             });
-    });
 
         new Setting(containerEl)
             .setName("Xournal++ installation path")
             .setDesc("The path where Xournal++ is installed (leave empty for system default).")
             .addText((toggle) => {
-                toggle
-                    .setValue(this.plugin.settings.xournalppPath)
-                    .onChange(async (value) => {
-                        this.plugin.settings.xournalppPath = value;
-                        await this.plugin.saveSettings();
-                    })
+                toggle.setValue(this.plugin.settings.xournalppPath).onChange(async (value) => {
+                    this.plugin.settings.xournalppPath = value;
+                    await this.plugin.saveSettings();
+                });
             });
-        
+
         new Setting(containerEl)
             .setName("Xournal++ template path")
             .setDesc(
@@ -92,22 +97,36 @@ export class XoppSettingsTab extends PluginSettingTab {
                     });
             });
 
-        new Setting(containerEl)
+        // Default Name
+        let defaultNameSetting = new Setting(containerEl)
             .setName("Default name for new Xournal++ files")
-            .setDesc(
-                "The default name for new Xournal++ files. Use `$fname` for currently opned file name."
-            )
             .addText((toggle) => {
                 toggle
                     .setValue(this.plugin.settings.defaultNewFileName)
-                    .setPlaceholder("e.g. $1")
+                    .setPlaceholder("e.g. ${MM}-${DD}")
                     .onChange(async (value) => {
                         this.plugin.settings.defaultNewFileName = value;
                         await this.plugin.saveSettings();
+
+                        descEl.setText(
+                            `The default name for new Xournal++ files. Example: "${parseFileName(value, this.plugin)}"`
+                        );
                     });
             });
+
+        const descEl = defaultNameSetting.setDesc(
+            `The default name for new Xournal++ files. Example: "${parseFileName(
+                this.plugin.settings.defaultNewFileName,
+                this.plugin
+            )}"`
+        ).descEl;
+
+        const titleEl = defaultNameSetting.nameEl;
+        const helpIcon = titleEl.createEl("span", { text: "❓", cls: "help-icon" });
+        helpIcon.style.cursor = "pointer";
+        helpIcon.style.marginLeft = "5px";
+        helpIcon.onclick = () => {
+            new ShorthandHelpModal(this.app, shorthands).open();
+        };
     }
-  
 }
-
-
