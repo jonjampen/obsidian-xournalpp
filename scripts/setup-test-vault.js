d405ebaa343beb98fffe80042b09fe9480526062
Jon Jampen <dev@jonjampen.ch>
2026-08-12 14:07:54 +0200

    feat: add automated test vault setup and esbuild integration for plugin development

---
+++ scripts/setup-test-vault.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+
+const destDir = path.join(__dirname, "../test-vault/.obsidian/plugins/hot-reload");
+
+if (!fs.existsSync(destDir)) {
+    fs.mkdirSync(destDir, { recursive: true });
+}
+
+function download(url, dest) {
+    const file = fs.createWriteStream(dest);
+    https
+        .get(url, (response) => {
+            response.pipe(file);
+            file.on("finish", () => {
+                file.close();
+                console.log(`Downloaded ${path.basename(dest)} to test-vault.`);
+            });
+        })
+        .on("error", (err) => {
+            fs.unlink(dest, () => {});
+            console.error(`Failed to download ${path.basename(dest)}:`, err.message);
+        });
+}
+
+// Download the official Hot Reload plugin files
+download("https://raw.githubusercontent.com/pjeby/hot-reload/master/main.js", path.join(destDir, "main.js"));
+download(
+    "https://raw.githubusercontent.com/pjeby/hot-reload/master/manifest.json",
+    path.join(destDir, "manifest.json")
+);
